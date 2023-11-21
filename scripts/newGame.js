ea75b33519695ae58bd3d0af1481f4518b13dde5
Jorge Alysson <jorgealysson201@gmail.com>
2023-11-21 20:55:34 -0300

    add file script newGame

---
+++ scripts/newGame.js
@@ -0,0 +1,18 @@
+const inputName = document.querySelector(".inputName");
+const newGameButton = document.querySelector(".newGameButton");
+const newGameForm= document.querySelector(".newGameForm");
+
+function validateInput(event) {
+    if (event.target.value.length >=3) {newGameButton.removeAttribute("disabled")}
+    else if (event.target.value.length <3) {newGameButton.setAttribute("disabled", "true")};
+}
+
+function handleSubmitNewGame(event) {
+    event.preventDefault();
+    localStorage.setItem("@memoryGame:playerName", inputName.value); //send key to local storage.
+    inputName.value=""; //
+    window.location.href = "pages/cards.html"
+}
+
+inputName.addEventListener("input",validateInput)
+newGameForm.addEventListener("submit",handleSubmitNewGame)
